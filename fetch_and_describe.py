9ce342df5c21cddd8bc22d2764c955b0815ac641
Scott_Lin <xunlin@bu.edu>
2018-09-11 22:53:21 -0400

    Combine image fetching and google vision

--- fetch_and_describe.py
+++ fetch_and_describe.py
@@ -4,6 +4,11 @@ import json
 import wget
 import os
 import sys
+import glob
+import io
+from google.cloud import vision
+from google.cloud.vision import types
+import csv
 
 consumer_key = ''
 consumer_secret = ''
@@ -86,3 +91,29 @@ except:
 for url in tweets_with_media:
 	wget.download(url)
 print ('\n\nFinished Downloading.\n')
+
+
+#Google vision
+print ('\n\nStart recognizing.\n')
+client = vision.ImageAnnotatorClient()
+print (os.getcwd())
+CURRENT_PATH = os.getcwd()
+image_paths = glob.glob(os.path.join(CURRENT_PATH, '*.jpg'))
+image_paths.sort()
+print ("Number of images: ", len(image_paths));
+
+for index, file_name in enumerate(image_paths):
+	with io.open(file_name, 'rb') as image_file:
+		content = image_file.read()
+	image = types.Image(content=content)
+	response = client.label_detection(image=image)
+	labels = response.label_annotations
+	print(index)
+	with open("output.csv", "a") as f:
+		writer = csv.writer(f)
+		row=[]
+		for label in labels:
+			print(label.description)
+			row.append(label.description)
+		writer.writerow(row)
+f.close()
